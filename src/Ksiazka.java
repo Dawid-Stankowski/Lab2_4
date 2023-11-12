f84a443f7de0ef6038553621abceb96c9968853d
Dawid-Stankowski <dawid441@interia.pl>
2023-11-12 13:20:20 +0100

    1.0

--- src/Ksiazka.java
+++ src/Ksiazka.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Ksiazka {
+public class Ksiazka {
+
+    String tytul;
+    String autor;
+    int rokWydania;
+    String wydawnictwo;
+
+    public Ksiazka(String tytul, String autor, int rokWydania, String wydawnictwo) {
+        this.tytul = tytul;
+        this.autor = autor;
+        this.rokWydania = rokWydania;
+        this.wydawnictwo = wydawnictwo;
+    }
+
+    public void wyswietlInformacje() {
+        System.out.println("Tytuł: " + tytul);
+        System.out.println("Autor: " + autor);
+        System.out.println("Rok wydania: " + rokWydania);
+        System.out.println("Wydawnictwo: " + wydawnictwo);
+    }
+
+    public static void main(String[] args) {
+        Ksiazka mojaKsiazka = new Ksiazka("Władca Pierścieni", "J.R.R. Tolkien", 1954, "Allen & Unwin");
+
+        mojaKsiazka.wyswietlInformacje();
+    }
 }
